37ca7d1fe6da0dd9a0da0105c6bb9a2546165ecb
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-04-03 00:04:01 +0200

    added ability to predict prices of phones

--- predict.js
+++ predict.js
@@ -1,47 +1,31 @@
 let nn = ml5.neuralNetwork({ task: "regression", debug: true });
 
-const battery = document.getElementById("battery");
-const screenSize = document.getElementById("screenSize");
-const rearcam = document.getElementById("rearcam");
-const ppi = document.getElementById("ppi");
-
 const btnPredict = document.getElementById("btnPredict");
 
 nn.load("/model/model.json", modelLoaded);
 
 function modelLoaded() {
   console.log("model is done loading");
+  btnPredict.addEventListener("click", () => makePrediction());
 }
 
-btnPredict.addEventListener("click", () => {
-  makePrediction().then((pred) => showPrediction(pred));
-});
+const makePrediction = async () => {
+  let battery = parseInt(document.getElementById("battery").value);
+  let rearcam = parseInt(document.getElementById("rearcam").value);
+  let resolution = parseInt(document.getElementById("resolution").value);
+  let ppi = parseInt(document.getElementById("ppi").value);
 
-async function makePrediction() {
-  let pred;
-  try {
-    const batteryVal = battery.value;
-    const screenSizeVal = screenSize.value;
-    const rearcamVal = rearcam.value;
-    const ppiVal = ppi.value;
+  let phone = {
+    battery: parseInt(battery),
+    rearcam: parseInt(rearcam),
+    resolution: parseInt(resolution),
+    ppi: parseInt(ppi),
+  };
 
-    const phone = {
-      battery: Number(batteryVal),
-      screenSize: Number(screenSizeVal),
-      rearcam: Number(rearcamVal),
-      ppi: Number(ppiVal),
-    };
-    console.log(phone);
+  const pred = await nn.predict(phone);
+  const price = pred[0].price;
+  const fmt = new Intl.NumberFormat("nl-NL", { style: "currency", currency: "EUR" });
+  const fixedPrice = fmt.format(price);
 
-    pred = await nn.predict(phone);
-  } catch (e) {
-    console.error(e);
-  }
-  return pred;
-}
-
-function showPrediction(pred) {
-  //   const fmt = new Intl.NumberFormat("nl-NL", { style: "currency", currency: "EUR" });
-  //   console.log(fmt.format(pred[0]));
-  console.log(pred);
-}
+  result.innerHTML = `The price of your phone is ${fixedPrice}`;
+};
